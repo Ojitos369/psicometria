364f93cf50be7160a465111609a11e1ed32c7923
Ojitos369 <ojitos369@gmail.com>
2020-09-02 11:23:41 -0500

    Primera version

---
+++ consulta.php
@@ -0,0 +1,35 @@
+<?php include("./php/conexion.php");?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/validacion.css">
+    <title>Ingresar</title>
+</head>
+<body>
+<div class="encabezado"><a href="index.php"><img src="./media/logo.jpg" alt="logo" class="logo"></a></div>
+    <div class="container">
+        <?php
+            if(isset($_GET['error'])){
+                $mensaje_error = "
+                    <div class=\"error\">
+                        <p>El nombre no coincide con el numero de identificacion o hay campos vacios que faltan por llenar</p>
+                    </div>";
+                echo $mensaje_error;
+            }
+        ?>
+        <form action="resultados.php" method="POST" class="form">
+            <div class="datos">
+                <input type="text" name="nombre" placeholder="Nombre completo" class="nombre">
+                <br>
+                <input type="number" name="identificacion" placeholder="No de identificado" class="identificacion">
+                <br>
+                <input type="submit" value="Consultar" class="entrar">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
